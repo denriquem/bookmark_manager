7f1e2fee32c388b0fe7d348771d8ce184728f730
Harry Mumford <harry_mumford@hotmail.co.uk>
2019-12-06 16:00:36 +0000

    get all tests to pass

--- spec/features/adding_bookmarks_spec.rb
+++ spec/features/adding_bookmarks_spec.rb
@@ -2,9 +2,9 @@ feature 'adding_bookmarks' do
   scenario 'it adds bookmarks' do
     visit '/add_form'
     # click_button 'Add Bookmark'
-    fill_in 'URL', with: 'Another Bookmark'
-    fill_in 'Title', with: 'Test Bookmark'
+    fill_in 'url', with: 'www.test_url.com'
+    fill_in 'title', with: 'Test Bookmark'
     click_button 'Add'
-    expect(page).to have_content('Another Bookmark')
+    expect(page).to have_link('Test Bookmark', href: "www.test_url.com")
   end
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,14 +1,13 @@
 feature "Viewing bookmarks" do
-  before :each do
-    add_row "http://www.makersacademy.com"
-    add_row "http://www.google.com"
-    add_row "http://www.destroyallsoftware.com"
-  end
+  # before :each do
+  #   add_row "http://www.makersacademy.com"
+  #   add_row "http://www.google.com"
+  #   add_row "http://www.destroyallsoftware.com"
+  # end
 
   scenario "Displays bookmarks" do
+    Bookmarks.add(url: 'http://www.makersacademy.com', title: 'Makers')
     visit '/bookmarks'
-    expect(page). to have_content("http://www.makersacademy.com")
-    expect(page). to have_content("http://www.google.com")
-    expect(page). to have_content("http://www.destroyallsoftware.com")
+    expect(page). to have_link('Makers', href: "http://www.makersacademy.com")
   end
 end
